9071d1ebe958662df2781537a5036813845acd36
sguest <spencer.b.guest@gmail.com>
2022-06-15 21:43:13 -0500

    Mod setup stuff

---
+++ src/main/java/sguest/jeimultiblocks/JeiMultiblocks.java
@@ -0,0 +1,8 @@
+package sguest.jeimultiblocks;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(JeiMultiblocks.MODID)
+public class JeiMultiblocks {
+    public static final String MODID = "jeimultiblocks";
+}
